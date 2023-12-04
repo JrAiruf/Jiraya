56aa7211330aa5fd0231183908a1f1334b792168
Jr_Silva_Dev <jos.pd2019@gmail.com>
2023-12-04 06:59:33 -0300

    Adding AuthModule to Main Module

--- Front/jiraya/lib/src/app_module.dart
+++ Front/jiraya/lib/src/app_module.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:jiraya/src/modules/auth/auth_module.dart';
 import 'package:jiraya/src/modules/initial/initial_module.dart';
 
 class AppModule extends Module {
   @override
   void routes(RouteManager r) {
     r.module("/", module: InitialModule());
+    r.module("/auth", module: AuthModule());
   }
 }
--- Front/jiraya/lib/src/modules/initial/splash_screen.dart
+++ Front/jiraya/lib/src/modules/initial/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   _goToAuth() async {
     await Future.delayed(const Duration(seconds: 7));
-    Modular.to.pushNamed("/auth/");
+    Modular.to.navigate("/auth/");
   }
 
   @override
